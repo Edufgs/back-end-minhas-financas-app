4289f6eeb8a3153bb25d53a5ff85f6d613689cdf
Eduardo Gonçalves da Silva <44234388+Edufgs@users.noreply.github.com>
2022-01-26 16:08:44 -0400

    Refatorando o UsuarioServiceTest utilizando Mock para simular instancias

--- src/test/java/com/edufgs/minhasfinancas/model/service/UsuarioServiceTest.java
+++ src/test/java/com/edufgs/minhasfinancas/model/service/UsuarioServiceTest.java
@@ -1,35 +1,52 @@
 package com.edufgs.minhasfinancas.model.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.edufgs.minhasfinancas.exception.RegraNegocioException;
-import com.edufgs.minhasfinancas.model.entity.Usuario;
 import com.edufgs.minhasfinancas.model.repository.UsuarioRepository;
 import com.edufgs.minhasfinancas.service.UsuarioService;
+import com.edufgs.minhasfinancas.service.impl.UsuarioServiceImpl;
 
 //Anotação para teste
-@SpringBootTest //Faz com que o contexto do spring boot teste suba
+//@SpringBootTest //No começo foi usada essa notação para testar mas depois de um tempo não precisa mais. Essa notação faz com que o contexto do spring boot teste suba para essa classe.
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test") //Seleciona o perfil do teste usando h2
 public class UsuarioServiceTest {
 	
-	@Autowired //Injeta essa classe no contexto do spring boot
+	//@Autowired //Injeta essa classe no contexto do spring boot. Não precisa mais pq vai ser feito testes unitarios e não precisa das instacias oficiais
 	UsuarioService service; //Interface que valida e testa os dados
 	
-	@Autowired //Injeta essa classe no contexto do spring boot
+	//@Autowired //Injeta essa classe no contexto do spring boot. Não precisa mais pq vai ser feito testes unitarios e não precisa das instacias oficiais
 	UsuarioRepository repository; //Banco de dados
+	
+	//Metodo para configurar os testes
+	@Before //Notação faz executar antes dos testes
+	public void setUp() {
+		//Classe mock onde ela cria instacias fake e simula que chamou algum metodo 
+		repository = Mockito.mock(UsuarioRepository.class); //Então ele vai simular os metodos para não precisar executar os reais
+		
+		//Instancia real do UsuarioService passando o repository mokado
+		service = new UsuarioServiceImpl(repository);
+	}
 	
 	/*(expected = Test.None.class) = Diz para o teste esperar que não lance nem uma exceção */
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
 		//cenario
+		//Classe mock onde ela cria instacias fake e simula que chamou algum metodo 
+		//UsuarioRepository usuarioRepositoryMock = Mockito.mock(UsuarioRepository.class); //Então ele vai simular os metodos para não precisar executar os reais
+		
+		//Quando fizer a chamada para o mocki repository o metodo existsByEmail passando qualquer string não interessando a string passada vai dar return false
+		//Resumindo: Quando chamar o metodo repository.existsByEmail passando qualquer string como parametro vai retornar false 
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);		
+		
 		//Exclui tudo do repositorio
-		repository.deleteAll();
+		//repository.deleteAll();
 		
 		//acao
 		//Esse metodo retorna uma exceção se tiver um email cadastrado, por isso foi colocado (expected = Test.None.class) para verificar se retorna uma exceção.
@@ -41,8 +58,11 @@ public class UsuarioServiceTest {
 	public void deveRetornarErroAoValidarEmailQuandoExistirEmailCadastrado(){
 		//cenario
 		//builder ajuda a constroir o objeto
-		Usuario usuario = Usuario.builder().nome("Usuario").email("email@email.com").build();
-		repository.save(usuario); //Salva no banco
+		//Usuario usuario = Usuario.builder().nome("Usuario").email("email@email.com").build();
+		//repository.save(usuario); //Salva no banco
+		
+		//Simulando com o mock
+		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 		
 		//acao
 		service.validarEmail("email@email.com");
